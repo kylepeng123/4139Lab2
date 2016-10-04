9acee05b35f178cade557546bc64dd71090dfd6a
kylepeng123 <kylepeng123@gmail.com>
2016-10-03 22:09:54 -0400

    made a small example

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
+router.post('/check_email_exists', function(req, result, next) {
+  var email = req.body.email;
+  //Go in to the database
+  //var decision = true;
+  result.send('OK');
+
+  console.log(email);
+});
+
 module.exports = router;
