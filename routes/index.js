e1dc520487531df2ee98f158a301509e576d1b07
kylepeng123 <kylepeng123@gmail.com>
2016-10-03 22:15:35 -0400

    modify to understand better

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
-router.post('/check_email_exists', function(req, result, next) {
+router.post('/check', function(req, result, next) {
   var email = req.body.email;
-  //Go in to the database
-  //var decision = true;
+
   result.send('OK');
 
   console.log(email);
